80a3897408b7dc34e7b370a9882178ba49d67083
yuri410 <canbitwell@gmail.com>
2023-05-07 16:28:51 +0800

    Update interface

--- torch/ops.h
+++ torch/ops.h
@@ -37,6 +37,20 @@ public:
 class RasterizeCudaContext : public RasterizeBaseContext
 {
 public:
+	/*
+	 * Create a new Cuda rasterizer context.
+	 *
+	 *  The context is deleted and internal storage is released when the object is
+	 *  destroyed.
+	 *
+	 *  Args:
+	 *    device (Optional): Cuda device on which the context is created. Type can be
+	 *                       `torch.device`, string (e.g., `'cuda:1'`), or int. If not
+	 *                       specified, context will be created on currently active Cuda
+	 *                       device.
+	 *  Returns:
+	 *    The newly created Cuda rasterizer context.
+	 */
 	RasterizeCudaContext(torch::Device device)
 		: RasterizeBaseContext(true)
 		, m_cpp_wrapper(GetCurrentCUDADeviceIndex(device))
@@ -59,22 +73,52 @@ public:
 		Mode_Manual
 	};
 
+	/*
+	 * Create a new OpenGL rasterizer context.
+	 *
+	 *	Creating an OpenGL context is a slow operation so you should usually reuse the same
+	 *	context in all calls to `rasterize()` on the same CPU thread. The OpenGL context
+	 *	is deleted when the object is destroyed.
+	 *
+	 *	Side note: When using the OpenGL context in a rasterization operation, the
+	 *	context's internal framebuffer object is automatically enlarged to accommodate the
+	 *	rasterization operation's output shape, but it is never shrunk in size until the
+	 *	context is destroyed. Thus, if you need to rasterize, say, deep low-resolution
+	 *	tensors and also shallow high-resolution tensors, you can conserve GPU memory by
+	 *	creating two separate OpenGL contexts for these tasks. In this scenario, using the
+	 *	same OpenGL context for both tasks would end up reserving GPU memory for a deep,
+	 *	high-resolution output tensor.
+	 *
+	 *	Args:
+	 *	  output_db (bool): Compute and output image-space derivates of barycentrics.
+	 *	  mode: OpenGL context handling mode. Valid values are 'manual' and 'automatic'.
+	 *	  device (Optional): Cuda device on which the context is created. Type can be
+	 *						 `torch.device`, string (e.g., `'cuda:1'`), or int. If not
+	 *						 specified, context will be created on currently active Cuda
+	 *						 device.
+	 *	Returns:
+	 *	  The newly created OpenGL rasterizer context.
+	 */
 	RasterizeGLContext(bool output_db, Mode mode, torch::Device device)
 		: RasterizeBaseContext(output_db)
 		, m_cpp_wrapper(output_db, mode == Mode_Automatic, GetCurrentCUDADeviceIndex(device))
 		, m_mode(mode)
 	{
-
+		
 	}
 
 	void set_context()
 	{
+		// Set (activate) OpenGL context in the current CPU thread.
+        // Only available if context was created in manual mode.
 		assert(m_mode == Mode_Manual);
 		m_cpp_wrapper.setContext();
 	}
 
 	void release_context()
 	{
+		// Release(deactivate) currently active OpenGL context.
+		// Only available if context was created in manual mode.
 		assert(m_mode == Mode_Manual);
 		m_cpp_wrapper.releaseContext();
 	}
@@ -144,30 +188,78 @@ public:
 	}
 };
 
+/*
+ * Rasterize triangles.
+ *
+ * All input tensors must be contiguous and reside in GPU memory except for
+ * the `ranges` tensor that, if specified, has to reside in CPU memory. The
+ * output tensors will be contiguous and reside in GPU memory.
+ *
+ * Args:
+ * 	glctx: Rasterizer context of type `RasterizeGLContext` or `RasterizeCudaContext`.
+ * 	pos: Vertex position tensor with dtype `torch.float32`. To enable range
+ * 		 mode, this tensor should have a 2D shape [num_vertices, 4]. To enable
+ * 		 instanced mode, use a 3D shape [minibatch_size, num_vertices, 4].
+ * 	tri: Triangle tensor with shape [num_triangles, 3] and dtype `torch.int32`.
+ * 	resolution: Output resolution as integer tuple (height, width).
+ * 	ranges: In range mode, tensor with shape [minibatch_size, 2] and dtype
+ * 			`torch.int32`, specifying start indices and counts into `tri`.
+ * 			Ignored in instanced mode.
+ * 	grad_db: Propagate gradients of image-space derivatives of barycentrics
+ * 			 into `pos` in backward pass. Ignored if using an OpenGL context that
+ * 			 was not configured to output image-space derivatives.
+ *
+ * Returns:
+ * 	A tuple of two tensors. The first output tensor has shape [minibatch_size,
+ * 	height, width, 4] and contains the main rasterizer output in order (u, v, z/w,
+ * 	triangle_id). If the OpenGL context was configured to output image-space
+ * 	derivatives of barycentrics, the second output tensor will also have shape
+ * 	[minibatch_size, height, width, 4] and contain said derivatives in order
+ * 	(du/dX, du/dY, dv/dX, dv/dY). Otherwise it will be an empty tensor with shape
+ * 	[minibatch_size, height, width, 0].
+ */
 inline torch::autograd::variable_list rasterize(RasterizeBaseContext* glctx, torch::Tensor pos, torch::Tensor tri, std::tuple<int, int> resolution, torch::Tensor ranges = torch::Tensor(), bool grad_db = true)
 {
 	grad_db &= glctx->m_output_db;
 
+	// Sanitize inputs.
 	if (ranges.size(0) == 0)
 	{
 		ranges = torch::empty({ 0,2 }, c10::TensorOptions(c10::ScalarType::Int)).cpu();
 	}
 
+	if (glctx->m_active_depth_peeler != nullptr)
+		throw std::exception("Cannot call rasterize() during depth peeling operation, use rasterize_next_layer() instead");
+
+	// Instantiate the function.
 	return _rasterize_func::apply(glctx, pos, tri, resolution, ranges, grad_db, -1);
 }
 
+// ----------------------------------------------------------------------------
+//  Depth peeler context manager for rasterizing multiple depth layers.
+// ----------------------------------------------------------------------------
 class DepthPeeler
 {
 public:
 	DepthPeeler(RasterizeBaseContext* glctx, torch::Tensor pos, torch::Tensor tri, std::tuple<int, int> resolution, torch::Tensor ranges = torch::Tensor(), bool grad_db = true)
 	{
+		/*
+		 * Create a depth peeler object for rasterizing multiple depth layers.
+		 * 
+		 * Arguments are the same as in `rasterize()`.
+		 * 
+		 * Returns:
+		 *   The newly created depth peeler.
+		 */
 		grad_db &= glctx->m_output_db;
 
+		// Sanitize inputs as usual.
 		if (ranges.size(0) == 0)
 		{
 			ranges = torch::empty({ 0,2 }, c10::TensorOptions(c10::ScalarType::Int)).cpu();
 		}
 
+		// Store all the parameters.
 		m_raster_ctx = glctx;
 		m_pos = pos;
 		m_tri = tri;
@@ -179,6 +271,11 @@ public:
 
 	void _enter()
 	{
+		if (m_raster_ctx == nullptr)
+			throw std::exception("Cannot re-enter a terminated depth peeling operation");
+		if (m_raster_ctx->m_active_depth_peeler != nullptr)
+			throw std::exception("Cannot have multiple depth peelers active simultaneously in a rasterization context");
+
 		m_raster_ctx->m_active_depth_peeler = this;
 		m_peeling_idx = 0;
 	}
@@ -192,6 +289,15 @@ public:
 
 	torch::autograd::variable_list rasterize_next_layer()
 	{
+		/*
+		 * Rasterize next depth layer.
+		 * 
+		 * Operation is equivalent to `rasterize()` except that previously reported
+		 * surface points are culled away.
+		 * 
+		 * Returns:
+		 *   A tuple of two tensors as in `rasterize()`.
+		 */
 		assert(m_peeling_idx >= 0);
 		torch::autograd::variable_list result = _rasterize_func::apply(m_raster_ctx, m_pos, m_tri, m_resolution, m_ranges, m_grad_db, m_peeling_idx);
 		m_peeling_idx++;
@@ -207,6 +313,11 @@ public:
 	int m_peeling_idx;
 };
 
+//----------------------------------------------------------------------------
+// Interpolate.
+//----------------------------------------------------------------------------
+
+// Output pixel differentials for at least some attributes.
 class _interpolate_func_da : public torch::autograd::Function<_interpolate_func_da>
 {
 public:
@@ -253,6 +364,7 @@ public:
 	}
 };
 
+// No pixel differential for any attribute.
 class _interpolate_func : public torch::autograd::Function<_interpolate_func>
 {
 public:
@@ -286,6 +398,36 @@ public:
 	}
 };
 
+/*
+ * Interpolate vertex attributes.
+ *
+ *	All input tensors must be contiguous and reside in GPU memory. The output tensors
+ *	will be contiguous and reside in GPU memory.
+ *
+ *	Args:
+ *		attr: Attribute tensor with dtype `torch.float32`.
+ *			  Shape is [num_vertices, num_attributes] in range mode, or
+ *			  [minibatch_size, num_vertices, num_attributes] in instanced mode.
+ *			  Broadcasting is supported along the minibatch axis.
+ *		rast: Main output tensor from `rasterize()`.
+ *		tri: Triangle tensor with shape [num_triangles, 3] and dtype `torch.int32`.
+ *		rast_db: (Optional) Tensor containing image-space derivatives of barycentrics,
+ *				 i.e., the second output tensor from `rasterize()`. Enables computing
+ *				 image-space derivatives of attributes.
+ *		diff_attrs: (Optional) List of attribute indices for which image-space
+ *					derivatives are to be computed. Special value 'all' is equivalent
+ *					to list [0, 1, ..., num_attributes - 1].
+ *
+ *	Returns:
+ *		A tuple of two tensors. The first output tensor contains interpolated
+ *		attributes and has shape [minibatch_size, height, width, num_attributes].
+ *		If `rast_db` and `diff_attrs` were specified, the second output tensor contains
+ *		the image-space derivatives of the selected attributes and has shape
+ *		[minibatch_size, height, width, 2 * len(diff_attrs)]. The derivatives of the
+ *		first selected attribute A will be on channels 0 and 1 as (dA/dX, dA/dY), etc.
+ *		Otherwise, the second output tensor will be an empty tensor with shape
+ *		[minibatch_size, height, width, 0].
+ */
 inline void interpolate(torch::Tensor attr, torch::Tensor rast, torch::Tensor tri, torch::Tensor rast_db = torch::Tensor(), bool diff_attrs_all = false, std::vector<int>* diff_attrs_list = nullptr)
 {
 	if (diff_attrs_list)
@@ -297,6 +439,10 @@ inline void interpolate(torch::Tensor attr, torch::Tensor rast, torch::Tensor tr
 		_interpolate_func::apply(attr, rast, tri);
 	}
 }
+
+//----------------------------------------------------------------------------
+// Texture
+//----------------------------------------------------------------------------
 
 enum class NVDRTextureFilterMode
 {
@@ -315,6 +461,7 @@ enum class NVDRTextureBoundaryMode
 	Cube
 };
 
+// Linear-mipmap-linear and linear-mipmap-nearest: Mipmaps enabled.
 class _texture_func_mip : public torch::autograd::Function<_texture_func_mip>
 {
 public:
@@ -401,6 +548,7 @@ public:
 	}
 };
 
+// Linear and nearest: Mipmaps disabled
 class _texture_func : public torch::autograd::Function<_texture_func>
 {
 public:
@@ -427,7 +575,7 @@ public:
 		torch::Tensor uv = ctx->get_saved_variables()[1];
 
 		torch::autograd::variable_list mip_stack;
-		for (size_t i = 4; i < ctx->get_saved_variables().size(); i++)
+		for (size_t i = 2; i < ctx->get_saved_variables().size(); i++)
 		{
 			mip_stack.push_back(ctx->get_saved_variables()[i]);
 		}
@@ -453,10 +601,59 @@ public:
 	}
 };
 
-static torch::autograd::variable_list texture(torch::Tensor tex, torch::Tensor uv,
+/*
+ * Perform texture sampling.
+ *
+ * All input tensors must be contiguous and reside in GPU memory. The output tensor
+ * will be contiguous and reside in GPU memory.
+ *
+ * Args:
+ * 	tex: Texture tensor with dtype `torch.float32`. For 2D textures, must have shape
+ * 		 [minibatch_size, tex_height, tex_width, tex_channels]. For cube map textures,
+ * 		 must have shape [minibatch_size, 6, tex_height, tex_width, tex_channels] where
+ * 		 tex_width and tex_height are equal. Note that `boundary_mode` must also be set
+ * 		 to 'cube' to enable cube map mode. Broadcasting is supported along the minibatch axis.
+ * 	uv: Tensor containing per-pixel texture coordinates. When sampling a 2D texture,
+ * 		must have shape [minibatch_size, height, width, 2]. When sampling a cube map
+ * 		texture, must have shape [minibatch_size, height, width, 3].
+ * 	uv_da: (Optional) Tensor containing image-space derivatives of texture coordinates.
+ * 		   Must have same shape as `uv` except for the last dimension that is to be twice
+ * 		   as long.
+ * 	mip_level_bias: (Optional) Per-pixel bias for mip level selection. If `uv_da` is omitted,
+ * 					determines mip level directly. Must have shape [minibatch_size, height, width].
+ * 	mip: (Optional) Preconstructed mipmap stack from a `texture_construct_mip()` call, or a list
+ * 					of tensors specifying a custom mipmap stack. When specifying a custom mipmap stack,
+ * 					the tensors in the list must follow the same format as `tex` except for width and
+ * 					height that must follow the usual rules for mipmap sizes. The base level texture
+ * 					is still supplied in `tex` and must not be included in the list. Gradients of a
+ * 					custom mipmap stack are not automatically propagated to base texture but the mipmap
+ * 					tensors will receive gradients of their own. If a mipmap stack is not specified
+ * 					but the chosen filter mode requires it, the mipmap stack is constructed internally
+ * 					and discarded afterwards.
+ * 	filter_mode: Texture filtering mode to be used. Valid values are 'auto', 'nearest',
+ * 				 'linear', 'linear-mipmap-nearest', and 'linear-mipmap-linear'. Mode 'auto'
+ * 				 selects 'linear' if neither `uv_da` or `mip_level_bias` is specified, and
+ * 				 'linear-mipmap-linear' when at least one of them is specified, these being
+ * 				 the highest-quality modes possible depending on the availability of the
+ * 				 image-space derivatives of the texture coordinates or direct mip level information.
+ * 	boundary_mode: Valid values are 'wrap', 'clamp', 'zero', and 'cube'. If `tex` defines a
+ * 				   cube map, this must be set to 'cube'. The default mode 'wrap' takes fractional
+ * 				   part of texture coordinates. Mode 'clamp' clamps texture coordinates to the
+ * 				   centers of the boundary texels. Mode 'zero' virtually extends the texture with
+ * 				   all-zero values in all directions.
+ * 	max_mip_level: If specified, limits the number of mipmaps constructed and used in mipmap-based
+ * 				   filter modes.
+ *
+ * Returns:
+ * 	A tensor containing the results of the texture sampling with shape
+ * 	[minibatch_size, height, width, tex_channels]. Cube map fetches with invalid uv coordinates
+ * 	(e.g., zero vectors) output all zeros and do not propagate gradients.
+ */
+inline torch::autograd::variable_list texture(torch::Tensor tex, torch::Tensor uv,
 	torch::Tensor uv_da = torch::Tensor(), torch::Tensor mip_level_bias = torch::Tensor(), const TextureMipWrapper* mip = nullptr,
 	NVDRTextureFilterMode filter_mode = NVDRTextureFilterMode::Auto, NVDRTextureBoundaryMode boundary_mode = NVDRTextureBoundaryMode::Wrap, int maxMipLevel = -1)
 {
+	// Default filter mode.
 	if (filter_mode == NVDRTextureFilterMode::Auto)
 	{
 		if (uv_da.size(0) || mip_level_bias.size(0))
@@ -465,9 +662,13 @@ static torch::autograd::variable_list texture(torch::Tensor tex, torch::Tensor u
 			filter_mode = NVDRTextureFilterMode::Linear;
 	}
 
+	assert(max_mip_level == -1 || max_mip_level >= 0);
+
+	// If mipping disabled via max level=0, we may as well use simpler filtering internally.
 	if (maxMipLevel == 0 && (filter_mode == NVDRTextureFilterMode::LinearMipmapNearest || filter_mode == NVDRTextureFilterMode::LinearMipmapLinear))
 		filter_mode = NVDRTextureFilterMode::Linear;
 
+	// Convert filter mode to internal enumeration
 	int filter_mode_enum = 0;
 	switch (filter_mode)
 	{
@@ -482,6 +683,7 @@ static torch::autograd::variable_list texture(torch::Tensor tex, torch::Tensor u
 		break;
 	}
 
+	// Convert boundary mode to internal enumeration
 	int boundary_mode_enum = 0;
 	switch (boundary_mode)
 	{
@@ -496,6 +698,7 @@ static torch::autograd::variable_list texture(torch::Tensor tex, torch::Tensor u
 		break;
 	}
 
+	// Construct a mipmap if necessary
 	TextureMipWrapper mip_wrapper;
 	torch::autograd::variable_list mip_stack;
 
@@ -517,6 +720,10 @@ static torch::autograd::variable_list texture(torch::Tensor tex, torch::Tensor u
 		return _texture_func::apply(filter_mode, tex, uv, filter_mode_enum, boundary_mode_enum);
 	}
 }
+
+// ----------------------------------------------------------------------------
+//  Antialias.
+// ----------------------------------------------------------------------------
 
 class _antialias_func : public torch::autograd::Function<_antialias_func>
 {
@@ -561,9 +768,36 @@ public:
 	}
 };
 
+/*
+ * Perform antialiasing.
+ *
+ *	All input tensors must be contiguous and reside in GPU memory. The output tensor
+ *	will be contiguous and reside in GPU memory.
+ *
+ *	Note that silhouette edge determination is based on vertex indices in the triangle
+ *	tensor. For it to work properly, a vertex belonging to multiple triangles must be
+ *	referred to using the same vertex index in each triangle. Otherwise, nvdiffrast will always
+ *	classify the adjacent edges as silhouette edges, which leads to bad performance and
+ *	potentially incorrect gradients. If you are unsure whether your data is good, check
+ *	which pixels are modified by the antialias operation and compare to the example in the
+ *	documentation.
+ *
+ *	Args:
+ *		color: Input image to antialias with shape [minibatch_size, height, width, num_channels].
+ *		rast: Main output tensor from `rasterize()`.
+ *		pos: Vertex position tensor used in the rasterization operation.
+ *		tri: Triangle tensor used in the rasterization operation.
+ *		topology_hash: (Optional) Preconstructed topology hash for the triangle tensor. If not
+ *					   specified, the topology hash is constructed internally and discarded afterwards.
+ *		pos_gradient_boost: (Optional) Multiplier for gradients propagated to `pos`.
+ *
+ *	Returns:
+ *		A tensor containing the antialiased image with the same shape as `color` input tensor.
+ */
 static torch::autograd::variable_list antialias(torch::Tensor color, torch::Tensor rast, torch::Tensor pos, torch::Tensor tri,
 	TopologyHashWrapper* _topology_hash = nullptr, float pos_gradient_boost = 1.0f)
 {
+	// Construct topology hash unless provided by user.
 	TopologyHashWrapper topology_hash;
 	if (_topology_hash)
 		topology_hash = antialias_construct_topology_hash(tri);
